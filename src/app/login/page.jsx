bdc302e6e2049ff84d6fe9e9763d02ad8dfdf17b
Jhanvi-Khare <jhanvikhare26@gmail.com>
2024-04-08 15:33:49 +0530

    react

---
+++ src/app/login/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import classes from './login.module.css';
+import icon from './myimage'
+
+const Login = () => {
+  return (
+    <div>
+        <h1>Login</h1>
+        <button className={classes.loginBtn}>Login Button</button>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Login;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <h1>Jhanvi</h1>
+        <h1 style={{ textAlign: 'center' , fontSize: 60 }}>Welcome to Homepage</h1>
+
+        <img src="/next.svg" alt="" />
+
+        <hr /><br /><input type="text" />
+        <button className='mybtn'>My Button</button>
+        <button className='loginBtn'>Lets Check</button>
     </div>
   )
 }
 
-export default Home
+export default Home;
